b14c2b4bb67e5224234eabe073abb4048ff80e95
Ricardo Gonzalez <ricargo.goonzalez@pickwin.net>
2018-04-08 15:52:17 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Repositorio para el taller de experimentacion interactiva :
+Presentado en el centro cultural españa mx
+
+
+ 2018
---
+++ schema.js
@@ -0,0 +1,64 @@
+propuestaArtistica : {
+  discurso:{
+    creaMemoria         : {},
+    evidencia           : {},
+    expresa             : {},
+    generaReflexion     : {},
+    informa             : {},
+    provoca             : {},
+  },
+  experiencia:{
+    concepto:{
+      actitud           : {},
+      conocimiento      : {},
+      imaginacion       : {},
+    }
+    contexto:{
+      politico            : {},
+      economico           : {},
+      social              : {},
+      tecnológico         : {},
+    }
+  },
+  interactor_interactividad:{
+    etapa1                : { tipo: 'selectivo' },
+    etapa2                : { tipo: 'selectivo' },
+    etapa3                : { tipo: 'transformativo' },
+    etapa4                : { tipo: 'transformativo' },
+    etapa5                : { tipo: 'constructivo' },
+  },
+  sensoperceptual:{
+    auditivo              : {},
+    gustativoOlfativo     : {},
+    haptico               : {},
+    orientacionBasica     : {},
+    visual                : {},
+  },
+  inteligenciasMultiples:{
+    linguistica           : {},
+    logico_matematica     : {},
+    visuo_espacial        : {},
+    musical               : {},
+    motor_cinética        : {},
+    interpersonal         : {},
+    intrapersonal         : {},
+    naturalista           : {},
+    existencialista       : {},
+  },
+  documentacion:{
+    imagen_fija             : {},
+    imagen_movimiento       : {},
+    descripcion__oral       : {},
+    descripcion_literaria   : {},
+  },
+  tecnica: {
+    graficos_por_ordenado   : {},
+    animaciones_3d          : {},
+    net_art                 : {},
+    software_art            : {},
+    hacktivismo             : {},
+    objetos_interctivos     : {},
+    fachadas_mediaticas     : {},
+    juegos_ordenador_arte   : {},
+  },
+},
